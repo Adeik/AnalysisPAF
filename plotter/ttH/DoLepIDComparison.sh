9f7d8792a69d9c18e84c86a479cf19a76577650b
Adeik <victoregoslorelua@gmail.com>
2017-04-05 00:38:04 +0200

    Implemented plotting of comparison between wp

--- plotter/ttH/DoLepIDComparison.sh
+++ plotter/ttH/DoLepIDComparison.sh
@@ -3,16 +3,54 @@ echo "%%%%%%%%%%%%%%%%%%%%%%% ttH ANALYSIS: COMPARISON BETWEEN TOP, STOP AND TTH
 echo ""
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Setting up the environment"
 source pre_start.sh
+if [ "$1" == "an" ]; then
+  echo "%%%%%> DONE"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting TOP analysis"
+  source DottHAnalysis.sh an $2 ttH_LIDtop
+  echo "%%%%%> DONE"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting STOP analysis"
+  source DottHAnalysis.sh an $2 ttH_LIDstop
+  echo "%%%%%> DONE"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting TTH analysis"
+  source DottHAnalysis.sh an $2 ttH_LIDtth
+elif [ "$1" == "pl" ]; then
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Setting up the environment"
+  source pre_start.sh
 
-echo "%%%%%> DONE"
-echo ""
-echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting TOP analysis"
-source DottHAnalysis.sh an $1 ttH_LIDtop
-echo "%%%%%> DONE"
-echo ""
-echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting STOP analysis"
-source DottHAnalysis.sh an $1 ttH_LIDstop
-echo "%%%%%> DONE"
-echo ""
-echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting TTH analysis"
-source DottHAnalysis.sh an $1 ttH_LIDtth
+  echo "%%%%%> DONE"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting to plot"
+  cd ..
+  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"top\")"
+  root -l -b -q "ttH/DrawPlots.C(\"All\",\"top\")"
+  
+  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"Stop\")"
+  root -l -b -q "ttH/DrawPlots.C(\"All\",\"Stop\")"
+  
+  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"ttH\")"
+  root -l -b -q "ttH/DrawPlots.C(\"All\",\"ttH\")"
+  cd ttH
+else
+    echo "ERROR - No valid arguments given"
+    echo "Please, execute this script with a valid argument"
+fi

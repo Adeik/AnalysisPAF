33196ce3f9b5841c117808b195adf8bedb3d3764
Adeik <victoregoslorelua@gmail.com>
2017-04-15 19:35:23 +0200

    Queue execution system implementation and debugging

--- plotter/ttH/DoLepIDComparison.sh
+++ plotter/ttH/DoLepIDComparison.sh
@@ -20,29 +20,9 @@ elif [ "$1" == "pl" ]; then
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Starting to plot"
   cd ..
-  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"top\")"
-  root -l -b -q "ttH/DrawPlots.C(\"All\",\"top\")"
-  
-  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"Stop\")"
-  root -l -b -q "ttH/DrawPlots.C(\"All\",\"Stop\")"
-  
-  root -l -b -q "ttH/DrawPlots.C(\"Muon\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"Elec\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"ElMu\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"2lSS\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"3l\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"4l\",\"ttH\")"
-  root -l -b -q "ttH/DrawPlots.C(\"All\",\"ttH\")"
+  source DottHAnalysis.sh pl top
+  source DottHAnalysis.sh pl Stop
+  source DottHAnalysis.sh pl ttH
   cd ttH
 else
     echo "ERROR - No valid arguments given"

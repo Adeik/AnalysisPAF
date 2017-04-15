899375f9abd2fa359f2545689150755b7c255763
Adeik <victoregoslorelua@gmail.com>
2017-04-15 19:51:41 +0200

    Debugging

--- plotter/ttH/QueueDottHAnalysis.sh
+++ plotter/ttH/QueueDottHAnalysis.sh
@@ -15,7 +15,7 @@ elif [ "$1" == "pl" ]; then
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
   echo ""
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
-  qsub -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/LEPIDCOMPARISONlogs -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/LEPIDCOMPARISONlogs -F $2 PlotThings.sh
+  qsub -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/LEPIDCOMPARISONlogs -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/LEPIDCOMPARISONlogs -F "$2" PlotThings.sh
 else
     echo "ERROR - No valid arguments given"
     echo "Please, execute this script with a valid argument"

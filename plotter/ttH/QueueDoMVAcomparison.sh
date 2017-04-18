eb8d4a3bc6ae994cedc7894d96df98af4556c5cd
Adeik <victoregoslorelua@gmail.com>
2017-04-18 18:41:09 +0200

    Implemented checking for missing root files when executing

--- plotter/ttH/QueueDoMVAcomparison.sh
+++ plotter/ttH/QueueDoMVAcomparison.sh
@@ -24,6 +24,22 @@ if [ "$1" == "an" ]; then
 
   MVAtth=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$MVAm DottHAnalysis.sh -F "an $2 ttH_LMVAtth")
   echo $MVAtth
+  
+  checkMVAet=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$MVAtth -F "$2 ttH_LMVAet /nfs/fanae/user/vrbouza/Documents/TFG/lepMVAcomparison/AnalysisPAF/ttH_temp/lepMVAcomparison/extratight" check.sh)
+  echo $checkMVAet
+  
+  checkMVAvt=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$checkMVAet -F "$2 ttH_LMVAvt /nfs/fanae/user/vrbouza/Documents/TFG/lepMVAcomparison/AnalysisPAF/ttH_temp/lepMVAcomparison/verytight" check.sh)
+  echo $checkMVAvt
+  
+  checkMVAt=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$checkMVAvt -F "$2 ttH_LMVAt /nfs/fanae/user/vrbouza/Documents/TFG/lepMVAcomparison/AnalysisPAF/ttH_temp/lepMVAcomparison/tight" check.sh)
+  echo $checkMVAt
+  
+  checkMVAm=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$checkMVAt -F "$2 ttH_LMVAm /nfs/fanae/user/vrbouza/Documents/TFG/lepMVAcomparison/AnalysisPAF/ttH_temp/lepMVAcomparison/medium" check.sh)
+  echo $checkMVAm
+
+  checkMVAtth=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$checkMVAm -F "$2 ttH_LMVAtth /nfs/fanae/user/vrbouza/Documents/TFG/lepMVAcomparison/AnalysisPAF/ttH_temp/lepMVAcomparison/tth" check.sh)
+  echo $checkMVAtth
+
   echo "%%%%%> DONE"
   echo ""
 elif [ "$1" == "pl" ]; then

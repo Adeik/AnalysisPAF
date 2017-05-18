ffca3898b9d2b83ef7af9b00e6f76181aab99eb1
Adeik <victoregoslorelua@gmail.com>
2017-05-18 16:51:35 +0200

    Debugging

--- plotter/ttH/DrawPlots.C
+++ plotter/ttH/DrawPlots.C
@@ -218,6 +218,7 @@ void DrawPlot(TString var, TString cut, TString chan, Int_t nbins, Float_t bin0,
   p->SetScaleMax(1.7);
   p->SetRatioMin(0);
   p->SetRatioMax(2);
+  p->doYieldsInLeg = false;
   if (counter != 0) {
     p->SetSignalStyle("H");
   }
